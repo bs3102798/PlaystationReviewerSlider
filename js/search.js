d8f3a8729e577345b17e3944677514fdd5c3ab5f
Burness Scott III <bsiii102798@gmail.com>
2024-08-06 16:42:28 -0700

    workingSearchScript

--- js/search.js
+++ js/search.js
@@ -1,3 +1,7 @@
 document.getElementById('searchForm').addEventListener('submit', function (event) {
     event.preventDefault();
+    const query = document.getElementById('searchInput').value.trim();
+    if (query) {
+        window.location.herf = `search.html?=${encodeURIComponent(query)}`
+    }
 })
--- js/searchResults.js
+++ js/searchResults.js
@@ -1,9 +1,32 @@
 const items = [
-    { name: 'uncharted', url: '' },
-    { name: 'unchartedLostLegcy', url: '' },
-    { name: 'RachedandClank', url: '' },
-    { name: 'Diablo', url: '' },
-    { name: 'GodOfWar', url: '' },
-    { name: 'Horizon' },
+    // { name: 'uncharted', url: '' },
+    // { name: 'unchartedLostLegcy', url: '' },
+    // { name: 'RachedandClank', url: '' },
+    // { name: 'Diablo', url: '' },
+    // { name: 'GodOfWar', url: '' },
+    // { name: 'Horizon' , url: 'Horizon.html' },
+    "Apple",
+    "Banana",
+    "Cherry",
+    "Date",
+    "Elderberry",
+    "Fig",
+    "Grape",
 
-]
+];
+
+const params = new URLSearchParams(window.location.search);
+const query = params.get('query') ? params.get('query').toLowerCase() : '';
+const resultsCOntainer = document.getElementById('results');
+
+const filteredItems = items.filter(item => item.toLowerCase().includes(query));
+
+if (filteredItems.length > 0) {
+    filteredItems.forEach(item => {
+        const itemElement = document.createElement('div');
+        itemElement.textContent = item;
+        resultsCOntainer.appendChild(itemElement);
+    })
+} else {
+    resultsCOntainer.textContent = 'Sorry, what you searched for cannot be found.'
+}
