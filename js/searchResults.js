628c470c953a8f0ff5535ae7b428bd2bb5a6a909
Burness Scott III <bsiii102798@gmail.com>
2024-08-07 22:33:15 -0700

    searchRoutes&mainRoutes

--- js/searchResults.js
+++ js/searchResults.js
@@ -1,32 +1,33 @@
 const items = [
-    // { name: 'uncharted', url: '' },
-    // { name: 'unchartedLostLegcy', url: '' },
-    // { name: 'RachedandClank', url: '' },
-    // { name: 'Diablo', url: '' },
-    // { name: 'GodOfWar', url: '' },
-    // { name: 'Horizon' , url: 'Horizon.html' },
-    "Apple",
-    "Banana",
-    "Cherry",
-    "Date",
-    "Elderberry",
-    "Fig",
-    "Grape",
+    { name: 'uncharted', url: '../searchHtml/uncharted.html' },
+    { name: 'unchartedLostLegcy', url: '../searchHtml/unchartedLostLegcy.html' },
+    { name: 'RachedandClank', url: '../searchHtml/Ratched&CLank.html' },
+    { name: 'Diablo', url: '../searchHtml/Diablo.html' },
+    { name: 'GodOfWar', url: '' },
+
+
+    { name: 'Horizon', url: '../searchHtml/Horizon.html' },
 
 ];
 
 const params = new URLSearchParams(window.location.search);
-const query = params.get('query') ? params.get('query').toLowerCase() : '';
+const query = params.get('query').toLowerCase();
 const resultsCOntainer = document.getElementById('results');
 
-const filteredItems = items.filter(item => item.toLowerCase().includes(query));
+//const filteredItems = items.filter(item => item.toLowerCase().includes(query));
 
-if (filteredItems.length > 0) {
-    filteredItems.forEach(item => {
-        const itemElement = document.createElement('div');
-        itemElement.textContent = item;
-        resultsCOntainer.appendChild(itemElement);
-    })
-} else {
+const matchingItem = items.find(item => item.name.toLowerCase() === query)
+
+if (matchingItem) {
+    window.location.href = matchingItem.url
+}
+//      {
+//     filteredItems.forEach(item => {
+//         const itemElement = document.createElement('div');
+//         itemElement.textContent = item;
+//         resultsCOntainer.appendChild(itemElement);
+//     })
+// }
+else {
     resultsCOntainer.textContent = 'Sorry, what you searched for cannot be found.'
 }
